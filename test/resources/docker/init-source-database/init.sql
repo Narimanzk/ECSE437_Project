c8b07e4b258049373b4d4826d89bfaf11d17f912
Nariman <nariman.zendehrooh@gmail.com>
2022-12-03 16:52:25 -0500

    updated test db scheme

--- test/resources/docker/init-source-database/init.sql
+++ test/resources/docker/init-source-database/init.sql
@@ -2,23 +2,23 @@ create schema if not exists test;
 
 CREATE TABLE test.movie
 (
-    movie       text    NOT NULL,
+    title       text    NOT NULL,
     director   text    NOT NULL,
     year        integer NOT NULL,
     genre text    NOT NULL
 );
 
-INSERT INTO test.movie (movie, director, year, genre)
+INSERT INTO test.movie (title, director, year, genre)
 values ('matrix', 'wachowski', 1999, 'action');
 
-INSERT INTO test.movie (movie, director, year, genre)
+INSERT INTO test.movie (title, director, year, genre)
 values ('goodfellas', 'scorsese', 1990, 'biography');
 
-INSERT INTO test.movie (movie, director, year, genre)
+INSERT INTO test.movie (title, director, year, genre)
 values ('se7en', 'fincher', 1995, 'crime');
 
-INSERT INTO test.movie (movie, director, year, genre)
+INSERT INTO test.movie (title, director, year, genre)
 values ('interstellar', 'nolan', 2014, 'adventure');
 
-INSERT INTO test.movie (movie, director, year, genre)
+INSERT INTO test.movie (title, director, year, genre)
 values ('psycho', 'hitchcock', 1960, 'horror');
